eb45ff3b8a8856cef797b6b70b49012d80311ff7
wouter856 <wouterwaumans@live.be>
2023-12-10 16:04:44 +0100

    feat: express and primus install

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const http = require('http');
+const WebSocketServer = require('websocket').server;
+
+//require express
+const express = require('express');
+
+const server = http.createServer((req, res) => {
+  console.log('request received');
+
+});
+
+const websocket = new WebSocketServer({
+  "httpServer": httpserver
+});
+
+websocket.on('request', request => {
+    conn = request.accept(null, request.origin);
+});
+
+
+server.listen(3000, () => {
+  console.log('server started at port 3000');
+});
