cd305debf19ce539f25465aa218edb2862de32f3
wouter856 <wouterwaumans@live.be>
2023-12-10 16:11:15 +0100

    feat: express setup

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-const http = require('http');
-const WebSocketServer = require('websocket').server;
-
-//require express
 const express = require('express');
+const http = require('http');
+const Primus = require('primus');
 
-const server = http.createServer((req, res) => {
-  console.log('request received');
+const app = express();
+const server = http.createServer(app);
+const primus = new Primus(server, { /* Primus configuration options */ });
 
+// Express route for regular HTTP requests
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
 });
 
-const websocket = new WebSocketServer({
-  "httpServer": httpserver
+// Primus WebSocket connection handling
+primus.on('connection', (spark) => {
+  console.log('Client connected:', spark.id);
+
+  spark.on('data', (data) => {
+    console.log('Received data from client:', data);
+  });
+
+  // Add more event handlers as needed
+
+  spark.write('Hello from the server!');
 });
 
-websocket.on('request', request => {
-    conn = request.accept(null, request.origin);
-});
-
-
-server.listen(3000, () => {
-  console.log('server started at port 3000');
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
 });
